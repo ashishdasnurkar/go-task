c92c79117666ca3ef0b7656421c60801e0c583d8
Thameera Senanayaka <me@thameera.com>
2023-09-04 19:44:04 +1000

    List tasks after adding

--- main.go
+++ main.go
@@ -6,6 +6,19 @@ import (
 	"strings"
 )
 
+var tasks []string
+
+func addTask(task string) {
+	tasks = append(tasks, task)
+	fmt.Println("Added task:", task)
+}
+
+func list() {
+	for i, task := range tasks {
+		fmt.Println(i+1, task)
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: go run main.go <COMMAND>")
@@ -14,6 +27,15 @@ func main() {
 
 	command := os.Args[1]
 
-	fmt.Println("Command:", command)
-	fmt.Println("Task:", strings.Join(os.Args[2:], " "))
+	if command == "add" {
+		if len(os.Args) < 3 {
+			fmt.Println("Usage: go run main.go add <TASK>")
+			os.Exit(1)
+		}
+
+		task := strings.Join(os.Args[2:], " ")
+		addTask(task)
+
+		list()
+	}
 }
