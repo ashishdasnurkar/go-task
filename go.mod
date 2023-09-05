6afe7fc02b53025edb06c99f65b9644ace946454
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2023-09-05 17:39:46 +1000

    persist tasks to db

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ashishdasnurkar/go-task
 
 go 1.21.0
+
+require (
+	github.com/google/uuid v1.3.1
+	github.com/mattn/go-sqlite3 v1.14.17
+)
--- main.go
+++ main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"os"
 	"strings"
+
+	"github.com/google/uuid"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var tasks []string
+var db *sql.DB
 
 func addTask(task string) {
 	tasks = append(tasks, task)
 	fmt.Println("Added task:", task)
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	id := uuid.New().String()
+
+	stmt, err := tx.Prepare("insert into tasks (uuid, description, done) values(?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(id, task, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 func list() {
@@ -20,6 +49,30 @@ func list() {
 }
 
 func main() {
+	var err error
+	db, err = sql.Open("sqlite3", "./tasks.db")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	// tasks table fields uuid, description, done
+
+	sqlStmt := `
+CREATE TABLE IF NOT EXISTS tasks (
+    uuid TEXT PRIMARY KEY,
+    description TEXT,
+    done BOOLEAN
+);
+`
+	_, err = db.Exec(sqlStmt)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: go run main.go <COMMAND>")
 		os.Exit(1)
